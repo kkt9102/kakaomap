1e788572c5987cf8cf88cbb0e413c63aaa70c8f5
kkt9102 <kkt9102@gmail.com>
2023-04-11 01:37:09 +0900

    사이드메뉴 페이지 이동시 닫힘 이벤트 추가

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 
 import { menuState } from "../state/commonState";
 
 import MenuItem from "../json/menu.json";
 
 const SideMenu = () => {
-  const menu = useRecoilValue(menuState);
+  const [menu, setMenu] = useRecoilState(menuState);
 
+  const handleCloseMenu = () => {
+    setMenu(false)
+  }
   return(
     <div className={`${menu !== true ? "" : "open"} sidemenu fixed `}>
       <ul className="flex flex_dir_c">
         {MenuItem.menu.map((item, index) =>
           <li key={index} className="flex flex_jc_s flex_ai_c flex_basis_100">
-            <Link to={item.link} className="relative flex flex_basis_100">{item.title}</Link>
+            <Link to={item.link} onClick={handleCloseMenu} className="relative flex flex_basis_100">{item.title}</Link>
           </li>
         )}
       </ul>
